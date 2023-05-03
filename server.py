b3fe16b9cfcfec4b68ba50c26c92e6252563deda
Lucas Gauna <lks.gauna@gmail.com>
2023-05-02 23:19:35 -0300

    se crearon las funciones basicas, faltan algunas.

--- server.py
+++ server.py
@@ -6,38 +6,95 @@ import os
 
 load_dotenv()
 
-def scrape_authenticated_page(url, username, password):
-    # Create a session object to persist the login cookies
-    session = requests.session()
+class SessionSysacad(object):
 
-    # Make a GET request to the login page to get the CSRF token
-    response = session.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'}).get("value") 
+    #Excepciones 
+    class AuthenticationError(Exception):
+        pass
+    
+    class OperationError(Exception):
+        pass
 
-   
+    #URLs a las que puede acceder la API
+    url = {
+        'default': 'Alumnos/menu/',
+        'examenes': 'Alumnos/Examenes/',
+        'cursado': 'Alumnos/Cursado/',
+        'login': ''
+        #posibles rutas a futuro:
+        #correlatividades
+        #certif alumno regular
+    }
 
-    # Prepare the login data and submit the POST request
-    login_data = {
+    #Constructor
+    def __init__(self, url_base, session=None):
+        self.url_base = url_base
+        if session:
+            assert isinstance(session, requests.Session), 'session debe ser una instancia de requests.Session'
+            self.session = session 
+        else:
+            self.session = requests.Session()
+
+    def _get(self,ext_url,data=None):
+        url = self.url_base + ext_url
+        response = self.session.get(url,data=data)
+    
+    #Extraer los datos de una tabla
+    def _data_from_table(bs_html, keys):
+        data = []
+        for tr in bs_html('tr'):
+            tds = {}
+            i = 0
+            for td in tr('td'):
+                tds[keys[i]] = td.getText()
+                i += 1
+            data.append(tds)
+        del data[0]
+        return data
+
+    # def _is_login_page(self, text):
+	# 	html = BeautifulSoup(text, 'lxml')
+	# 	if html.title.string == u'Ingreso Alumnos al SYSACAD' or html('p', attrs={'class': "textoError"}):
+	# 		return True
+	# 	return False
+        
+    def login(self, legajo, password):
+
+        response = self._get(self.url['login'])
+        html = BeautifulSoup(response.content, 'html.parser')
+        csrf_token = html.find('input', {'name': 'csrfmiddlewaretoken'}).get("value")
+        
+        print(response)
+
+        login_data = {
         'csrfmiddlewaretoken': csrf_token,
         'radio': 'A', # Assuming the user is always an "Alumno"
-        'username': username,
+        'username': legajo,
         'password': password,
         'submit': 'Acceder',
-    }
-    session.post(url, data=login_data)
+        }
 
-    # Make a GET request to the page that requires authentication
-    response = session.get('https://sysacadweb.frre.utn.edu.ar/Alumnos/Cursado/')
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # Parse the content of the page and extract the data you need
+        response = self.session.post(self.url_base, data=login_data)
+        print(response)
 
-    return soup
+    def get_examenes(self):
+        data={}
+        response = self._get(self.url['examenes'])
+        html = BeautifulSoup(response.content, 'html.parser')
 
-if __name__ == '__main__': 
-    start = time.time()
-    soup = scrape_authenticated_page('https://sysacadweb.frre.utn.edu.ar/', '25446', os.environ['PASSWORD'])
-    # print all the response 
-    # print(soup)
-    end = time.time()
-    print(end - start)
+        #Parsear la tabla de examenes
+        keys = (    'Fecha', 
+                    'Materia', 
+                    'Nota', 
+                    'Especialidad', 
+                    'Plan', 
+                    'Codigo'
+                )
+        data['examenes'] = self._data_from_table(html,keys)
+        for examen in data['examenes']:
+            examen
+
+
+
+
+
--- test2.py
+++ test2.py
@@ -1,18 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+from dotenv.main import load_dotenv
+import os
 
-# Make a request to the page and get the response content
-url = 'https://sysacadweb.frre.utn.edu.ar/'
-response = requests.get(url)
-content = response.content
+load_dotenv()
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(content, 'html.parser')
+def _data_from_table(bs_html, keys):
+		data = []
+		for tr in bs_html('tr'):
+			tds = {}
+			i = 0
+			for td in tr('td'):
+				tds[keys[i]] = td.getText()
+				i += 1
+			data.append(tds)
+		del data[0]
+		
+		return data
 
-# Look for a CSRF token field in the form
-csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'})
+def scrape_authenticated_page(url, username, password):
+    # Create a session object to persist the login cookies
+    session = requests.session()
 
-if csrf_token is not None:
-    print('CSRF token found:', csrf_token['value'])
-else:
-    print('No CSRF token found')
+    # Make a GET request to the login page to get the CSRF token
+    response = session.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'}).get("value") 
+
+   
+
+    # Prepare the login data and submit the POST request
+    login_data = {
+        'csrfmiddlewaretoken': csrf_token,
+        'radio': 'A', # Assuming the user is always an "Alumno"
+        'username': username,
+        'password': password,
+        'submit': 'Acceder',
+    }
+    session.post(url, data=login_data)
+
+    # Make a GET request to the page that requires authentication
+    response = session.get('https://sysacadweb.frre.utn.edu.ar/Alumnos/Examenes/')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Parse the content of the page and extract the data you need
+
+    return soup
+
+if __name__ == '__main__': 
+    start = time.time()
+    soup = scrape_authenticated_page('https://sysacadweb.frre.utn.edu.ar/', '25446', os.environ['PASSWORD'])
+    # print all the response 
+    keys = ('Fecha', 'Materia', 'Nota', 'Especialidad', 'Plan', 'Codigo')
+    lista = _data_from_table(soup,keys)
+    print(lista)
+    end = time.time()
+    print(end - start)
